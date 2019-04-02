fb88c9c7c2f1b610c698aa8127af5c36bc2c9d0c
sayaligaikawad <gaikawad.s@northeastern.edu>
2019-04-01 20:32:06 -0400

    Added CRD and updated types

---
+++ pkg/controller/add_application.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/sayaligaikawad/csye7374-operator/pkg/controller/application"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, application.Add)
+}
